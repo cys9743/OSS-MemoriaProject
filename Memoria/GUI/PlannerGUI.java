9c31cd006464689a295e2d1d69025e3cd6906631
choiyunseong <cys9743@gmail.com>
2020-06-08 20:37:55 +0900

    Planner.GUI 색상 변경 및 폰트 교체

--- Memoria/GUI/PlannerGUI.java
+++ Memoria/GUI/PlannerGUI.java
@@ -51,7 +51,9 @@ public class PlannerGUI {
 	 */
 	private void initialize() {
 		frmMemoriaplanner = new JFrame();
-		frmMemoriaplanner.setForeground(Color.DARK_GRAY);
+		frmMemoriaplanner.getContentPane().setBackground(Color.DARK_GRAY);
+		frmMemoriaplanner.getContentPane().setForeground(Color.DARK_GRAY);
+		frmMemoriaplanner.setForeground(Color.GRAY);
 		frmMemoriaplanner.setFont(new Font("Edwardian Script ITC", Font.BOLD | Font.ITALIC, 12));
 		frmMemoriaplanner.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Administrator\\Desktop\\63376930-\uBB3C-\uD22C\uBA85-\uD55C-\uC0C1\uD488-\uC6D0\uD65C\uD55C-\uD328\uD134\uBE44 -.jpg"));
 		frmMemoriaplanner.setType(Type.UTILITY);
@@ -61,32 +63,39 @@ public class PlannerGUI {
 		frmMemoriaplanner.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Daliy Plan");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setFont(new Font("����", Font.BOLD | Font.ITALIC, 12));
 		lblNewLabel.setBounds(157, 0, 71, 23);
 		frmMemoriaplanner.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("\uC815\uB82C");
-		lblNewLabel_1.setForeground(Color.BLACK);
+		lblNewLabel_1.setForeground(Color.WHITE);
 		lblNewLabel_1.setBounds(262, 23, 35, 15);
 		frmMemoriaplanner.getContentPane().add(lblNewLabel_1);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("\uB0B4\uC6A9 ");
+		chckbxNewCheckBox.setBackground(Color.LIGHT_GRAY);
 		chckbxNewCheckBox.setBounds(8, 57, 376, 23);
 		frmMemoriaplanner.getContentPane().add(chckbxNewCheckBox);
 		
 		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("\uC624\uD508\uC18C\uC2A4 \uD504\uB85C\uC81D\uD2B8 \uACFC\uC81C");
+		chckbxNewCheckBox_1.setBackground(Color.LIGHT_GRAY);
 		chckbxNewCheckBox_1.setBounds(8, 82, 376, 23);
 		frmMemoriaplanner.getContentPane().add(chckbxNewCheckBox_1);
 		
 		JCheckBox chckbxNewCheckBox_1_1 = new JCheckBox("New check box");
+		chckbxNewCheckBox_1_1.setBackground(Color.LIGHT_GRAY);
 		chckbxNewCheckBox_1_1.setBounds(8, 107, 376, 23);
 		frmMemoriaplanner.getContentPane().add(chckbxNewCheckBox_1_1);
 		
 		JCheckBox chckbxNewCheckBox_1_2 = new JCheckBox("New check box");
+		chckbxNewCheckBox_1_2.setBackground(Color.LIGHT_GRAY);
 		chckbxNewCheckBox_1_2.setBounds(8, 132, 376, 23);
 		frmMemoriaplanner.getContentPane().add(chckbxNewCheckBox_1_2);
 		
 		JCheckBox chckbxNewCheckBox_1_3 = new JCheckBox("New check box");
+		chckbxNewCheckBox_1_3.setBackground(Color.LIGHT_GRAY);
 		chckbxNewCheckBox_1_3.setBounds(8, 157, 376, 23);
 		frmMemoriaplanner.getContentPane().add(chckbxNewCheckBox_1_3);
 		
